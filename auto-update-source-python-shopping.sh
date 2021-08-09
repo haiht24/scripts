f068c631116ec6af384bedb06a0f6115204e63c8
HaiHT <haiht369@gmail.com>
2021-08-09 11:56:03 +0700

    first

---
+++ auto-update-source-python-shopping.sh
@@ -0,0 +1,20 @@
+# stop python app before update
+
+# update
+cd /tmp;
+# remove old
+rm -rf python3-api-shopping-master;
+rm master.zip
+# download new one
+wget https://github.com/haiht24/python3-api-shopping/archive/master.zip;
+unzip master.zip;
+
+cd /home/*;
+mv python-api python-api.old;
+mv /tmp/python3-api-shopping-master /home/*
+mv /home/*/python3-api-shopping-master python-api
+
+# install requirements.txt
+source /home/*/virtualenv/python-api/3.8/bin/activate && cd /home/*/python-api
+pip install --upgrade pip
+pip install -r requirements.txt
